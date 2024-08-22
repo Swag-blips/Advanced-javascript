201f2497558331f44c80c2aa72aad4d8ea6d5c6e
Swag-blips <coderblip@gmail.com>
2024-08-22 22:29:16 +0100

    mysterious keyword

---
+++ this-keyword/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+class Cat {
+  constructor(firstName) {
+    this.firstName = firstName;
+  }
+
+  dance(style = "tango") {
+    return `Meow, i am ${this.firstName} and i like to ${style}`;
+  }
+}
+
+const fluffy = new Cat("fluffy");
+
+// storing this in a variable would result in an error
+console.log(fluffy.dance());
+
+
+///
