5fc9217eccf13f2c55fadcd1216375c4e9c1501e
Swag-blips <coderblip@gmail.com>
2024-08-24 15:43:55 +0100

    updated files

--- this-keyword/apply.js
+++ this-keyword/apply.js
@@ -1,24 +1,30 @@
-const conan = {
-  name: "Conan",
-  city: "Los Angeles",
-  sing: function () {
-    console.log("THIS is:", this);
-    return `${this.name} sings LA LA LA`;
-  },
-};
+// const conan = {
+//   name: "Conan",
+//   city: "Los Angeles",
+//   sing: function () {
+//     console.log("THIS is:", this);
+//     console.log(`${this.name} sings LA LA LA`);
+//   },
+// };
 
-const lisa = {
-  name: "Lisa",
-  city: "Los Angeles",
-};
+// const lisa = {
+//   name: "Lisa",
+//   city: "Los Angeles",
+// };
 
-const lisaSing = conan.sing.bind(lisa);
+// const lisaSing = conan.sing.bind(lisa);
 
-lisaSing();
+// lisaSing();
 
-function applySalesTax(taxRate, price) {
-  return price + price * taxRate;
-}
+// function applySalesTax(taxRate, price) {
+//   return price + price * taxRate;
+// }
 
-const applyCASalesTax = applySalesTax.bind(null, 0.0725);
-const applyMTSalesTax = applySalesTax.bind(null, 0);
+// const applyCASalesTax = applySalesTax.bind(null, 0.0725);
+// const applyMTSalesTax = applySalesTax.bind(null, 0);
+
+// ///////
+
+// const btn = document.querySelector("#clickMe");
+
+// btn.addEventListener("click", conan.sing.bind(conan));
