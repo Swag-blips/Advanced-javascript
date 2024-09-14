3e0d8320b75a717fe7ba20ff44f9d77f8be2f8ff
Swag-blips <coderblip@gmail.com>
2024-09-14 23:46:11 +0100

    changes

--- oop-under-the-hood/script.js
+++ oop-under-the-hood/script.js
@@ -1,26 +1,38 @@
-// class Dog {
-//   constuctor(name, breed) {
-//     this.name = name;
-//     this.breed = breed;
-//   }
+class Dog {
+  constructor(name, breed) {
+    this.name = name;
+    this.breed = breed;
+  }
 
-//   bark() {
-//     return "Wooft";
-//   }
+  bark() {
+    return `${this.name} says woof`;
+  }
+  sleep() {
+    return `${this.name} is sleeping`;
+  }
+}
+
+const elton = new Dog("elton", "aussie");
+const wyatt = new Dog("wyatt", "aussie");
+
+// function Dog(name, breed) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.bark = function () {
+//     return `${this.name} says woof`;
+//   };
+//   this.sleep = function () {
+//     return `${this.name} is sleeping`;
+//   };
 // }
 
-function Dog(name, breed) {
-  this.name = name;
-  this.breed = breed;
-}
+// const elton = new Dog("elton", "german sherperd");
+// const shelby = new Dog("shelby", "localedog");
 
-const elton = new Dog("elton", "german sherperd");
-const shelby = new Dog() * ("shelby", "localedog");
+// function User(username, email) {
+//   this.username = username;
+//   this.email = email;
+//   this.isAdmin = false;
+// }
 
-function User(username, email) {
-  this.username = username;
-  this.email = email;
-  this.isAdmin = false;
-}
-
-const wayne = new User("wayne", "wayne@test.com");
+// const wayne = new User("wayne", "wayne@test.com");
