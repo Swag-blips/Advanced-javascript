2120fc49c998af58011b618008ea185ebd608163
Swag-blips <coderblip@gmail.com>
2024-09-19 23:23:58 +0100

    changes

--- oop-under-the-hood/script.js
+++ oop-under-the-hood/script.js
@@ -15,19 +15,6 @@ class Dog {
 const elton = new Dog("elton", "aussie");
 const wyatt = new Dog("wyatt", "aussie");
 
-// function Dog(name, breed) {
-//   this.name = name;
-//   this.breed = breed;
-//   this.bark = function () {
-//     return `${this.name} says woof`;
-//   };
-//   this.sleep = function () {
-//     return `${this.name} is sleeping`;
-//   };
-// }
-
-// const elton = new Dog("elton", "german sherperd");
-// const shelby = new Dog("shelby", "localedog");
 
 // function User(username, email) {
 //   this.username = username;
