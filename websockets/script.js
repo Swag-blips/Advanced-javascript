cd82a12fa97cc516142ec51bf56df3d8838876d1
Swag-blips <coderblip@gmail.com>
2024-12-04 20:34:37 +0100

    websockets server init

---
+++ websockets/script.js
@@ -0,0 +1,4 @@
+// 1 Web sockets is a duplex bi directional persistent connection between a client and a server, they are persistent
+// 2 useful for real time communication, online games, chat applications
+// 3 Web sockets have low latency
+// 4 Without web sockets, http long polling is utilized, which just makes repetitive requests over and over again, down side is, latency issues
