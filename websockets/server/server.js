3c1bbe51cb1f91af02e7007c60b88d0672b5eed1
Swag-blips <coderblip@gmail.com>
2024-12-04 21:17:30 +0100

    feat:trying to serve hmtl file

--- websockets/server/server.js
+++ websockets/server/server.js
@@ -1,10 +1,14 @@
 import express from "express";
-
+import path from "path";
 const app = express();
 
 app.use(express.static("static/"));
 
-app.get(":/roomName", (req, res, next) => {
-  res.sendFile(`${__dirname}/chat.html
-        `);
+app.get("/:roomName", (req, res, next) => {
+  res.sendFile(`${path.join("/chat.html")}`);
+  return;
+});
+
+app.listen(3000, () => {
+  console.log(`server is runnng on port 3000`);
 });
---
+++ websockets/static/js/chat.js
@@ -0,0 +1,3 @@
+// open websocket connection from the browser to the server
+
+// DO stuff eventually
