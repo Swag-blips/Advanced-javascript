d438e6f83b22dfea34cab851cf96ade6e897b567
Swag-blips <coderblip@gmail.com>
2024-12-04 22:17:44 +0100

    feat: init exress websockets

--- websockets/server/server.js
+++ websockets/server/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import path from "path";
+import expressWS from "express-ws";
+
 const app = express();
 
-app.use(express.static("static/"));
+expressWS(app);
+app.use(express.static(path.join(import.meta.dirname, "../static")));
 
 app.get("/:roomName", (req, res, next) => {
-  res.sendFile(`${path.join("/chat.html")}`);
+  res.sendFile(`${path.join(import.meta.dirname, "../chat.html")}`);
   return;
 });
 
+app.ws("/chat/:roomName", (ws, req, next) => {
+  ws.send(Math.random());
+  // try {
+
+  // } catch (error) {
+
+  // }
+});
 app.listen(3000, () => {
   console.log(`server is runnng on port 3000`);
 });
