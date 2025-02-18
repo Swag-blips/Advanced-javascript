fca4ec4818456d262772c0f77460df6335b69794
Swag-blips <coderblip@gmail.com>
2025-02-18 14:47:10 +0100

    feat:setup files, parse files to an AST and traverse with babel/traverse

--- webpack/bundler.js
+++ webpack/bundler.js
@@ -1,9 +1,20 @@
 const fs = require("fs");
-
+const babylon = require("babylon");
+const traverse = require("@babel/traverse").default;
 function createAsset(filename) {
   const content = fs.readFileSync(filename, "utf-8");
 
-  console.log(content);
+  const ast = babylon.parse(content, {
+    sourceType: "module",
+  });
+
+  console.log(ast);
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      console.log(node);
+    },
+  });
 }
 
-createAsset("./entry");
+createAsset("./entry.js");
